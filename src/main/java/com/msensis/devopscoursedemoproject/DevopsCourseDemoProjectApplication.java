4c49baa0a697c3cdae925d765df3151a75fab88d
Giannis Oikonomou <i.oikonomou@msensis.com>
2026-06-04 19:30:01 +0300

    initial commit

---
+++ src/main/java/com/msensis/devopscoursedemoproject/DevopsCourseDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.msensis.devopscoursedemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCourseDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCourseDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msensis/devopscoursedemoproject/DevopsCourseDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msensis.devopscoursedemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCourseDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
