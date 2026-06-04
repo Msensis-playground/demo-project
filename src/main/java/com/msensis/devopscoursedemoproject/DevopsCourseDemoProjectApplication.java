44eff47611682738967791972808cd4df2bcc59a
Giannis Oikonomou <i.oikonomou@msensis.com>
2026-06-04 22:17:54 +0300

    Changes for my new feature

--- src/main/java/com/msensis/devopscoursedemoproject/DevopsCourseDemoProjectApplication.java
+++ src/main/java/com/msensis/devopscoursedemoproject/DevopsCourseDemoProjectApplication.java
@@ -8,6 +8,7 @@ public class DevopsCourseDemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsCourseDemoProjectApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
